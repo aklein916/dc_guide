59a7d2a3fd22ee75e4813a3a839cc57cf4bf7c83
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-21 09:46:20 -0400

    basic controller files created

---
+++ app/controllers/neighborhoods_controller.rb
@@ -0,0 +1,15 @@
+class NeighborhoodsController < ApplicationController
+
+  def index
+    @neighborhoods = Neighborhood.all
+  end
+
+  def show
+    @neighborhood = Neighborhood.find(params[:id])
+  end
+
+  def new
+    @neighborhood = Neighborhood.new
+  end
+
+end
