b8f8c7259ea5cc90d36f7a66e81dd4ae5e9cd27d
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-21 12:56:51 -0400

    seeded file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "neighborhoods#index"
-  resources :neighborhoods, only [:index, :show, :create, :new]
-  resources :restaurants, only [:index, :show, :new, :create, :edit]
+  resources :neighborhoods, only: [:index, :show, :create, :new]
+  resources :restaurants, only: [:index, :show, :new, :create, :edit]
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+neighborhoods = Neighborhood.create([{ name: 'Adams Morgan', nickname: 'AdMo', quadrant: 'NW', photo_url: 'http://behbg.com/wp-content/uploads/2015/07/adams-morgan.jpg' }, {name: 'Dupont Circle', nickname: 'Dupont', quadrant: 'NW', photo_url: 'http://www.toptravel.us/wp-content/uploads/2015/05/Dupont-Circle1_full.jpg' },
+{name: 'Logan Circle', nickname: 'Logan', quadrant: 'NW', photo_url: 'http://www.wcsmith.com/wp-content/uploads/2014/05/logan_circle_Gallery_Item_1_Image_1.jpeg'}, {name: '14th St Corridor', nickname: '14th', quadrant: 'NW', photo_url: '' } ])
+
+restaurants = Restaurant.create([{name: 'Commissary', cuisine:
+'American', pricepoint: '$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}, {name: 'Birch and Barley', cuisine:
+'American', pricepoint: '$$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'},
+{name: 'Kapnos', cuisine: 'Greek Tapas', pricepoint: '$$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}, {name: 'Barcelona', cuisine:
+'Spanish Tapas', pricepoint: '$$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}, {name: 'Floriana', cuisine:
+'French', pricepoint: '$$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}, {name: 'Sushi', cuisine:
+'Sushi', pricepoint: '$$$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}, {name: 'Beefsteak', cuisine:
+'American', pricepoint: '$', review: 'fdfdnsfkdsfdkslfjsdkfjdsklfjdsklfjdsklfj'}])
