a5440e0a95759457370e7dc474728958ecf38e2d
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-21 09:32:06 -0400

    schema file generated and files migrated

--- db/migrate/20160318183621_create_neighborhoods.rb
+++ db/migrate/20160318183621_create_neighborhoods.rb
@@ -1,6 +1,10 @@
 class CreateNeighborhoods < ActiveRecord::Migration
   def change
     create_table :neighborhoods do |t|
+      t.string :name
+      t.string :nickname
+      t.string :quadrant
+      t.string :photo_url
     end
   end
 end
