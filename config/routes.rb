c30dc74b38b6daeb351d62a6cd79ec6facb67d23
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-21 15:46:12 -0400

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  roots to "workouts#index"
+
+  resources :workouts, only: [:index, :workouts, :contact, :show]
+  resources :places, only: [:index, :show, :new ]
 
 end
